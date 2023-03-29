84035f31a5f3a10f203ef4cec487880cbb7b4aae
Max Kellermann <mk@cm4all.com>
2023-03-29 11:41:23 +0200

    MysqlProtocol: add Int2, Int6, Int8

--- src/MysqlProtocol.hxx
+++ src/MysqlProtocol.hxx
@@ -16,6 +16,16 @@
 
 namespace Mysql {
 
+struct Int2 {
+	uint8_t data[2];
+
+public:
+	constexpr operator uint_least32_t() const noexcept {
+		return static_cast<uint_least32_t>(data[0]) |
+			(static_cast<uint_least32_t>(data[1]) << 8);
+	}
+};
+
 struct Int3 {
 	uint8_t data[3];
 
@@ -36,6 +46,36 @@ public:
 			(static_cast<uint_least32_t>(data[1]) << 8) |
 			(static_cast<uint_least32_t>(data[2]) << 16) |
 			(static_cast<uint_least32_t>(data[3]) << 24);
+	}
+};
+
+struct Int6 {
+	uint8_t data[6];
+
+public:
+	constexpr operator uint_least64_t() const noexcept {
+		return static_cast<uint_least64_t>(data[0]) |
+			(static_cast<uint_least64_t>(data[1]) << 8) |
+			(static_cast<uint_least64_t>(data[2]) << 16) |
+			(static_cast<uint_least64_t>(data[3]) << 24) |
+			(static_cast<uint_least64_t>(data[4]) << 32) |
+			(static_cast<uint_least64_t>(data[5]) << 40);
+	}
+};
+
+struct Int8 {
+	uint8_t data[8];
+
+public:
+	constexpr operator uint_least64_t() const noexcept {
+		return static_cast<uint_least64_t>(data[0]) |
+			(static_cast<uint_least64_t>(data[1]) << 8) |
+			(static_cast<uint_least64_t>(data[2]) << 16) |
+			(static_cast<uint_least64_t>(data[3]) << 24) |
+			(static_cast<uint_least64_t>(data[4]) << 32) |
+			(static_cast<uint_least64_t>(data[5]) << 40) |
+			(static_cast<uint_least64_t>(data[6]) << 48) |
+			(static_cast<uint_least64_t>(data[7]) << 56);
 	}
 };
 

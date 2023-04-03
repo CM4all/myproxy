804bbbbbb78a8b17bd7f88e4b8cbb7001f188089
Max Kellermann <mk@cm4all.com>
2023-04-03 22:22:02 +0200

    MysqlProtocol: add more client capability flags

--- src/MysqlProtocol.hxx
+++ src/MysqlProtocol.hxx
@@ -27,13 +27,38 @@ enum class ErrorCode : uint_least16_t {
 	HANDSHAKE_ERROR = 1043,
 };
 
+static constexpr uint_least32_t CLIENT_MYSQL = 1;
+static constexpr uint_least32_t CLIENT_FOUND_ROWS = 2;
+static constexpr uint_least32_t CLIENT_LONG_FLAG = 4;
 static constexpr uint_least32_t CLIENT_CONNECT_WITH_DB = 8;
+static constexpr uint_least32_t CLIENT_NO_SCHEMA = 16;
+static constexpr uint_least32_t CLIENT_COMPRESS = 32;
+static constexpr uint_least32_t CLIENT_ODBC = 64;
+static constexpr uint_least32_t CLIENT_LOCAL_FILES = 128;
+static constexpr uint_least32_t CLIENT_IGNORE_SPACE = 256;
 static constexpr uint_least32_t CLIENT_PROTOCOL_41 = 512;
+static constexpr uint_least32_t CLIENT_INTERACTIVE = 1024;
+static constexpr uint_least32_t CLIENT_SSL = 2048;
+static constexpr uint_least32_t CLIENT_IGNORE_SIGPIPE = 4096;
 static constexpr uint_least32_t CLIENT_TRANSACTIONS = 8192;
+static constexpr uint_least32_t CLIENT_RESERVED = 16384;
+static constexpr uint_least32_t CLIENT_SECURE_CONNECTION = 32768;
+static constexpr uint_least32_t CLIENT_MULTI_STATEMENTS = 1UL << 16;
+static constexpr uint_least32_t CLIENT_MULTI_RESULTS = 1UL << 17;
+static constexpr uint_least32_t CLIENT_PS_MULTI_RESULTS = 1UL << 18;
 static constexpr uint_least32_t CLIENT_PLUGIN_AUTH = 1UL << 19;
 static constexpr uint_least32_t CLIENT_CONNECT_ATTRS = 1UL << 20;
 static constexpr uint_least32_t CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = 1UL << 21;
+static constexpr uint_least32_t CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS = 1UL << 22;
 static constexpr uint_least32_t CLIENT_SESSION_TRACK = 1UL << 23;
+static constexpr uint_least32_t CLIENT_DEPRECATE_EOF = 1UL << 24;
+static constexpr uint_least32_t CLIENT_OPTIONAL_RESULTSET_METADATA = 1UL << 25;
+static constexpr uint_least32_t CLIENT_ZSTD_COMPRESSION = 1UL << 26;
+static constexpr uint_least32_t CLIENT_QUERY_ATTRIBUTES = 1UL << 27;
+static constexpr uint_least32_t MULTI_FACTOR_AUTHENTICATION = 1UL << 28;
+static constexpr uint_least32_t CLIENT_PROGRESS = 1UL << 29;
+static constexpr uint_least32_t CLIENT_SSL_VERIFY_SERVER_CERT = 1UL << 30;
+static constexpr uint_least32_t CLIENT_REMEMBER_OPTIONS = 1UL << 31;
 
 struct Int2 {
 	uint8_t data[2];

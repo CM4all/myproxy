0a8fe7bfe09f6849eadc34a8567ae6be3a4caabb
Max Kellermann <mk@cm4all.com>
2023-04-04 14:26:30 +0200

    LHandler: verify that the handler is a function

--- src/LHandler.cxx
+++ src/LHandler.cxx
@@ -4,7 +4,11 @@
 
 #include "LHandler.hxx"
 
+#include <stdexcept>
+
 LuaHandler::LuaHandler(lua_State *L, Lua::StackIndex idx)
 	:on_handshake_response(L, idx)
 {
+	if (!lua_isfunction(L, Lua::GetStackIndex(idx)))
+		throw std::invalid_argument{"Not a function"};
 }

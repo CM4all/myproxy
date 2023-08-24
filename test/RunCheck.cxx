363253afff6813ce4f6bc6ded470bd80e7fefa90
Max Kellermann <mk@cm4all.com>
2023-08-24 14:53:44 +0200

    test/RunCheck: new debug program

---
+++ test/RunCheck.cxx
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright CM4all GmbH
+// author: Max Kellermann <mk@cm4all.com>
+
+#include "Check.hxx"
+#include "Options.hxx"
+#include "event/Loop.hxx"
+#include "net/AddressInfo.hxx"
+#include "net/Resolver.hxx"
+#include "util/Cancellable.hxx"
+#include "util/PrintException.hxx"
+
+#include <fmt/format.h>
+
+int
+main(int argc, char **argv) noexcept
+try {
+	if (argc != 4) {
+		fmt::print(stderr, "Usage: {} ADDRESS USER PASSWORD\n",
+			   argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	const auto address = Resolve(argv[1], 3306, 0, SOCK_STREAM);
+
+	const CheckOptions options {
+		.user = argv[2],
+		.password = argv[3],
+		.no_read_only = true,
+	};
+
+	EventLoop event_loop;
+
+	struct Handler final : public CheckServerHandler {
+		bool finished = false;
+		CheckServerResult result;
+
+		void OnCheckServer(CheckServerResult _result) noexcept override {
+			result = _result;
+			finished = true;
+		}
+	} handler;
+
+	CancellablePointer cancel_ptr;
+
+	CheckServer(event_loop, address.GetBest(), options,
+		    handler, cancel_ptr);
+
+	if (!handler.finished)
+		event_loop.Run();
+
+	return EXIT_SUCCESS;
+} catch (...) {
+	PrintException(std::current_exception());
+	return EXIT_FAILURE;
+}

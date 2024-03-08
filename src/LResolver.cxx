c144479809ef883ac5fe43d666c6c768c5be8edc
Max Kellermann <mk@cm4all.com>
2024-03-08 12:10:39 +0100

    LResolver: move code to libcommon

--- src/LResolver.cxx
+++ src/LResolver.cxx
@@ -9,6 +9,7 @@
 #include "lua/Error.hxx"
 #include "lua/ForEach.hxx"
 #include "lua/PushCClosure.hxx"
+#include "lua/net/Resolver.hxx"
 #include "lua/net/SocketAddress.hxx"
 #include "net/Resolver.hxx"
 #include "net/AddressInfo.hxx"
@@ -20,35 +21,6 @@ extern "C" {
 }
 
 #include <string.h>
-
-static constexpr auto hints = MakeAddrInfo(0, AF_UNSPEC, SOCK_STREAM);
-
-static int
-l_mysql_resolve(lua_State *L)
-{
-	if (lua_gettop(L) != 1)
-		return luaL_error(L, "Invalid parameter count");
-
-	const char *s = luaL_checkstring(L, 1);
-
-	if (*s == '/' || *s == '@') {
-		AllocatedSocketAddress address;
-		address.SetLocal(s);
-		Lua::NewSocketAddress(L, std::move(address));
-		return 1;
-	}
-
-	AddressInfoList ai;
-
-	try {
-		ai = Resolve(s, 3306, &hints);
-	} catch (...) {
-		Lua::RaiseCurrent(L);
-	}
-
-	Lua::NewSocketAddress(L, std::move(ai.GetBest()));
-	return 1;
-}
 
 static int
 l_mysql_cluster(lua_State *L)
@@ -89,7 +61,11 @@ void
 RegisterLuaResolver(lua_State *L, EventLoop &event_loop)
 {
 	Cluster::Register(L);
-	Lua::SetGlobal(L, "mysql_resolve", l_mysql_resolve);
+
+	static constexpr auto hints = MakeAddrInfo(0, AF_UNSPEC, SOCK_STREAM);
+	Lua::PushResolveFunction(L, hints, 3306);
+	lua_setglobal(L, "mysql_resolve");
+
 	Lua::SetGlobal(L, "mysql_cluster",
 		       Lua::MakeCClosure(l_mysql_cluster,
 					 Lua::LightUserData{&event_loop}));

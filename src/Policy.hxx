0ac7d6403d833b8c2b7d0805cb14eb5c93344541
Max Kellermann <max.kellermann@ionos.com>
2025-05-27 20:31:53 +0200

    update my email address

--- src/Policy.hxx
+++ src/Policy.hxx
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 // Copyright CM4all GmbH
-// author: Max Kellermann <mk@cm4all.com>
+// author: Max Kellermann <max.kellermann@ionos.com>
 
 /*
  * The policy implementation.  It decides what to do with a request.
